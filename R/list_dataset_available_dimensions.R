62e18092ca312446863eef6e164e2c8a71a2e8d1
ptaconet <paul.taconet@ird.fr>
2017-10-05 15:55:13 +0200

    correction bug

--- R/list_dataset_available_dimensions.R
+++ R/list_dataset_available_dimensions.R
@@ -26,6 +26,8 @@
 
 list_dataset_available_dimensions<-function(con,dataset_metadata){
   
+  if(nrow(dataset_metadata)==0){stop("There is no dataset that corresponds to your query")}
+  
   db_dimensions_parameters<-read.csv(system.file("extdata", "db_dimensions_parameters.csv",package="rtunaatlas"),stringsAsFactors = F)
   
   # get type of variable of the dataset
@@ -51,11 +53,12 @@ list_dataset_available_dimensions<-function(con,dataset_metadata){
   unique_values_dimension<-dbGetQuery(con,paste0("SELECT DISTINCT ", available_dimensions_in_dataset$db_fact_table_colname[i] ," FROM ",table_name," WHERE id_metadata=",id_metadata))
   
   # the condition hereunder filters the columns that are not null (i.e. do not have only UNKNOWN values)
-  if(!(nrow(unique_values_dimension==1) & unique_values_dimension[1,1]==0)) {
-    available_dimensions_in_dataset_not_null<-c(available_dimensions_in_dataset_not_null, available_dimensions_in_dataset$dimension[i])
+  if (nrow(unique_values_dimension)==1) {
+    if (unique_values_dimension[1,1]==0) { }} else { 
+      available_dimensions_in_dataset_not_null<-c(available_dimensions_in_dataset_not_null, available_dimensions_in_dataset$dimension[i]) }
+  
   } 
   
-  }
   
 return(available_dimensions_in_dataset_not_null)  
  
