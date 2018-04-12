2cfeee0919b0903b560b33ac3a4fc1147b05d49b
Paul Taconet <paul.taconet@gmail.com>
2018-04-12 18:37:16 +0200

    add function to create regular grids and calendars

---
+++ R/create_calendar.R
@@ -0,0 +1,72 @@
+#' @name create_calendar
+#' @aliases create_calendar
+#' @title Creates a continious calendar 
+#' @description Creates a continious calendar according to input parameters : first and final date of the calendar, temporal resolution and temporal resolution unit
+#' @export
+#'
+#' @usage create_calendar(firstdate,finaldate,resolution,unit)
+#'  
+#' @param firstdate character string first date of the calendar, format : YYYY-MM-DD
+#' @param finaldate character string final date of the calendar, format : YYYY-MM-DD
+#' @param resolution real temporal resolution of the calendar in day, month or year (see: temporal_reso_unit). Note: for 1/2 month put temporal_reso=1/2 and temporal_reso_unit="month" , type = integer;
+#' @param unit character string temporal resolution unit. accepted value : "day" "month" "year"
+#'
+#' @return A dataframe with two columns : "time_start" (starting date of the period) and "time_end" (ending date of the period).
+#' 
+#' @details 
+#' 
+#' Values for unit can be: "day","month","year". 
+#' 
+#' The algorithm can create a mi-month calendar : first period between 1st and 15th of a month, second period between 16th and the end of the month. For this set resolution=0.5 and unit="month"
+#' 
+#' @family process data
+#' 
+#' @examples
+#' 
+#' calendar<-create_calendar("1900-01-01","2020-01-01",15,"day")
+#' calendar<-create_calendar("1900-01-01","2020-01-01",0.5,"month")
+#' 
+#' @import lubridate
+#' 
+#' @author Chloé Dalleau, \email{dalleau.chloe@@hotmail.fr}
+#'   
+
+create_calendar <- function(firstdate,finaldate,resolution,unit){
+
+  ### Initialisation
+  firstdate <- as_date(firstdate, tz = "UTC")
+  finaldate <- as_date(finaldate, tz = "UTC")
+  ### Create calendar based on input parameters
+  if (resolution==1/2 && unit=="month") {
+    ### Case : months are cut in two periods : first one from 1 to 15th, second one from 16 to month end
+    ### initialisation
+    temp_step <- 1
+    day(firstdate) <- 01
+    month(finaldate)<- month(finaldate)+1
+    finaldate <- rollback(finaldate)
+    mi_month <- firstdate
+    day(mi_month) <- 15
+    ### create the first date of each month
+    pre_calendar_1 <- seq(firstdate, finaldate, paste(temp_step, unit))
+    ### create the 15th of each month
+    pre_calendar_2 <- seq(mi_month, finaldate, paste(temp_step, unit))
+    ### create the calendar end
+    end_calendar <- c(pre_calendar_1[length(pre_calendar_1)] + months(temp_step)-days(1))
+    ### merge in a calendar, time_start = period beginnig, time_end = period end
+    calendar <- data.frame(time_start=sort(c(pre_calendar_1,pre_calendar_2+1)),time_end=sort(c(pre_calendar_1[2:length(pre_calendar_1)]-1,pre_calendar_2, end_calendar)))
+  } else {
+    ### Other case
+    ### create the period of time
+    pre_calendar <- seq(firstdate, finaldate, paste(resolution, unit))
+    ### create the calendar end
+    switch(unit,
+           "day" = {end_calendar <- pre_calendar[length(pre_calendar)] + days(resolution)-days(1)}, 
+           "month" = { end_calendar <- pre_calendar[length(pre_calendar)] + months(resolution)-days(1)},
+           "year" = {end_calendar <- pre_calendar[length(pre_calendar)] + years(resolution)-days(1)}
+    )
+    ### merge in a calendar, time_start = period beginnig, time_end = period end
+    calendar <- data.frame(time_start=pre_calendar,time_end=c(pre_calendar[2:length(pre_calendar)]-1, end_calendar))
+  }
+  
+  return(calendar)
+}
---
+++ R/create_grid.R
@@ -0,0 +1,57 @@
+#' @name create_grid
+#' @aliases create_grid
+#' @title Creates a continious grid 
+#' @description Create a continious (regular) geospatial grid
+#' @export
+#'
+#' @usage create_grid(latmin,latmax,lonmin,lonmax,resolution,crs,centred)
+#'  
+#' @param latmin real smallest latitude for the spatial grid in degree (range: -90 to 90)
+#' @param latmax real biggest latitude wanted for the spatial grid in degree (range: -90 to 90)
+#' @param lonmin real smallest longitude for the spatial grid in degree (range: -180 to 180)
+#' @param lonmax real biggest longitude wanted for the spatial grid in degree (range: -180 to 180)
+#' @param resolution real spatial resolution of the grid in degree
+#' @param crs character string a character string of projection arguments; the arguments must be entered exactly as in the PROJ.4 documentation.
+#' @param centred boolean TRUE for a grid centred on zero. default is TRUE
+#'
+#' @return A SpatialPolygon object (package : sp) based on input parameters and composed by square polygons.
+#' 
+#' @details 
+#' 
+#' The default CRS is EPSG 4326: "+init=epsg:4326 +proj=longlat +datum=WGS84"
+#' 
+#' @family process data
+#' 
+#' @examples
+#' 
+#' grid <- create_grid(latmin=-90,latmax=90,lonmin=-180,lonmax=80,resolution=1,centred=T)
+#' 
+#' @import sp
+#' 
+#' @author Chloé Dalleau, \email{dalleau.chloe@@hotmail.fr}
+#'   
+
+  create_grid <- function(latmin=-90,latmax=90,lonmin=-180,lonmax=180,resolution,crs="+init=epsg:4326 +proj=longlat +datum=WGS84",centred=T){
+  
+  ### Creates polygons grid
+  ## if lat and lon are equal to [-90,90] and [-180,180], it create error in spTransform (infinite value) in the code part : "Select polygones which are covered by trajectories"
+    if (latmin== -90){latmin=latmin+resolution}
+    if (latmax== 90){latmax=latmax-resolution}
+    if (lonmin== -180){lonmin=lonmin+resolution}
+    if (lonmax== 180){lonmax=lonmax-resolution}
+    ## treatment
+    cellsdimlat <-  ceiling(abs(latmax-latmin)/resolution)
+    cellsdimlon <-  ceiling(abs((lonmax-lonmin)/resolution))
+    ## create a centered grid 
+    if (centred==T){
+      smallest_lon <- ceiling(lonmin/resolution)*resolution + resolution/2
+      smallest_lat <- ceiling(latmin/resolution)*resolution + resolution/2
+    } else {
+      smallest_lon <- lonmin + resolution/2
+      smallest_lat <- latmin + resolution/2
+    }
+    grid = GridTopology(cellcentre.offset=c(smallest_lon,smallest_lat), cellsize=c(resolution,resolution), cells.dim=c(cellsdimlon,cellsdimlat))
+    sp_zone <- as.SpatialPolygons.GridTopology(grid, proj4string = CRS(crs))
+    
+    return(sp_zone)
+  }
