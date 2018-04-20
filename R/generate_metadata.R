84eb4877502fb0191f5886740583ca0dce8ff847
Paul Taconet <paul.taconet@gmail.com>
2018-04-20 15:03:09 +0200

    very minor correction

--- R/generate_metadata.R
+++ R/generate_metadata.R
@@ -59,7 +59,7 @@ for (i in 1:length(metadata_columns_input)){
 for (i in 1:length(metadata_columns_input)){
   this_metadata_element<-metadata_file[metadata_columns_input[i]][[1]]
   words_to_replace<-gsub("[\\%\\%]", "", regmatches(this_metadata_element, gregexpr("\\%.*?\\%", this_metadata_element))[[1]])
-  if (length(words_to_replace>0)){
+  if (length(words_to_replace)>0){
     for (j in 1:length(words_to_replace)){
       if (words_to_replace[j] %in% colnames(metadata_file)){
         metadata_file[metadata_columns_input[i]]<-gsub(paste0("%",words_to_replace[j],"%"),metadata_file[words_to_replace[j]][[1]],metadata_file[metadata_columns_input[i]][[1]])
