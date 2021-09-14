ad479cf2004f6aac0f1f9a67840e8466c6af5afa
juldebar <juldebar@gmail.com>
2021-09-14 11:12:48 +0200

    improve comments for Level 1 processing

--- R/convert_units.R
+++ R/convert_units.R
@@ -262,11 +262,12 @@ convert_units<-function(con,df_input, df_conversion_factor, codelist_geoidentifi
   
   stats<-merge(stats_before_conversion,stats_after_conversion)
   
-  stats %>% group_by(unit)%>%
-    summarise(sum_unit_source_before_conversion=sum(sum_unit_source_before_conversion))
-  
-  stats %>% group_by(unit_target)%>%
-    summarise(sum_unit_target_after_conversion=sum(sum_unit_target_after_conversion))
+  # @juldebar => commenting lines below since not used to generate the output
+  # stats %>% group_by(unit)%>%
+  #   summarise(sum_unit_source_before_conversion=sum(sum_unit_source_before_conversion))
+  # 
+  # stats %>% group_by(unit_target)%>%
+  #   summarise(sum_unit_target_after_conversion=sum(sum_unit_target_after_conversion))
   
   
   #stats_after_conversion<-df_input %>% 
